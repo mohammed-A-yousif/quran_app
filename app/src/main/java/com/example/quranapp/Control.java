41cd271e51e7bc95f3bd92f4123a8d85b54b2eb2
mohammed-A-yousif <71412422+mohammed-A-yousif@users.noreply.github.com>
2020-10-13 10:15:28 +0200

    finalized

--- app/src/main/java/com/example/quranapp/Control.java
+++ app/src/main/java/com/example/quranapp/Control.java
@@ -13,9 +13,10 @@ public class Control extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controler);
-        CardView teach_cardView =  findViewById(R.id.teach_cardView);
+        CardView teach_cardView = findViewById(R.id.teach_cardView);
         CardView students_cardView = findViewById(R.id.students_cardView);
         CardView missions_cardView = findViewById(R.id.stud_missions_cardView);
+        CardView review_cardView = findViewById(R.id.student_review_cardView);
 
 
         teach_cardView.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +39,14 @@ public class Control extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), Missions.class);
+                startActivity(i);
+            }
+        });
+
+        review_cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Review.class);
                 startActivity(i);
             }
         });
