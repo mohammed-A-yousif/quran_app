e700cb657ba9533304c822a1a1af2928bd34c79b
Ahmed K Madani <ahmedkamal543@gmail.com>
2020-10-20 10:55:07 +0200

    add teacher api

--- app/src/main/java/com/example/quranapp/AddingTeacher.java
+++ app/src/main/java/com/example/quranapp/AddingTeacher.java
@@ -2,24 +2,104 @@ package com.example.quranapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class AddingTeacher extends AppCompatActivity {
+
+    EditText teacher_name_editText;
+    EditText teacher_living_editText;
+    EditText teacher_phone_editText;
+    EditText teacher_password_editText;
+    EditText teacher_AcademicLevel_editText;
+
+    String teacher_name_;
+    String teacher_living_;
+    String teacher_phone_;
+    String teacher_password_;
+    String teacher_AcademicLevel_;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.adding_teacher);
+
+
+        teacher_name_editText = findViewById(R.id.teacher_name_editText);
+        teacher_living_editText = findViewById(R.id.teacher_living_editText);
+        teacher_phone_editText = findViewById(R.id.teacher_phone_editText);
+        teacher_password_editText = findViewById(R.id.teacher_password_editText);
+//        teacher_AcademicLevel_editText = findViewById(R.id.teacher_AcademicLevel_editText);
+
         Button addTeacherButton = findViewById(R.id.add_teacher_button);
 
         addTeacherButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                teacher_name_ = teacher_name_editText.getText().toString();
+                teacher_living_ = teacher_living_editText.getText().toString();
+                teacher_phone_ = teacher_phone_editText.getText().toString();
+                teacher_password_ = teacher_password_editText.getText().toString();
+//                teacher_AcademicLevel_ = teacher_AcademicLevel_editText.getText().toString();
+
+                AddTeacher();
                 Intent i = new Intent(getApplicationContext(), Teach.class);
                 startActivity(i);
             }
         });
+    }
+
+    private void AddTeacher() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,  URLs.AddTeacher + "?IdAdmin=" + SharedPrefManager.getInstance(this).getAdmin().getId() + "&Name=" + teacher_name_ , null,
+                (JSONObject response) -> {
+                    try {
+                        String name = response.getString("Name");
+
+                        Log.d("res", response.toString());
+                        onInsertSuccess();
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        onInsertFailed();
+                    }
+
+                    Log.d("String Response : ", ""+  response.toString());
+                    Log.d("name", String.valueOf(SharedPrefManager.getInstance(this).isLoggedIn()));
+                }, error -> Log.d("Error getting response", "" +error));
+
+        requestQueue.add(jsonObjectRequest);
+        Log.d("rs", "" + jsonObjectRequest);
+
+    }
+
+    private void onInsertFailed() {
+//        viewDialog.hideDialog();
+        Snackbar.make(findViewById(android.R.id.content), "Sign in Failed", Snackbar.LENGTH_LONG)
+                .setAction("Try Again", v -> {
+                    AddTeacher(PhoneNumber, Password);
+                }).show();
+    }
+
+    private void onInsertSuccess() {
+//        viewDialog.hideDialog();
+        Snackbar.make(findViewById(android.R.id.content), "Sign in Successfully", Snackbar.LENGTH_LONG)
+                .show();
+        startActivity(new Intent(this, Control.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/quranapp/URLs.java
+++ app/src/main/java/com/example/quranapp/URLs.java
@@ -5,6 +5,8 @@ public class URLs {
     public  static String BaseUrl = "http://10.0.2.2:5000/";
 
     public static String Login = BaseUrl + "admin_login/";
+    public static String AddTeacher = BaseUrl + "teacher_register/";
+
 
     public static String getBaseUrl() {
         return BaseUrl;
