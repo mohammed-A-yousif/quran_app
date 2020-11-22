4d59b6a3bf0e4e883aea6fddf3a6741c2d03cc23
Ahmed K Madani <ahmedkamal543@gmail.com>
2020-11-22 21:50:52 +0200

    fix the control desgin

--- app/src/main/java/com/example/quranapp/Control.java
+++ app/src/main/java/com/example/quranapp/Control.java
@@ -3,6 +3,7 @@ package com.example.quranapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +14,16 @@ public class Control extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controler);
+
         CardView teach_cardView = findViewById(R.id.teach_cardView);
         CardView students_cardView = findViewById(R.id.students_cardView);
+
         CardView missions_cardView = findViewById(R.id.stud_missions_cardView);
         CardView review_cardView = findViewById(R.id.student_review_cardView);
 
+        TextView UserName = findViewById(R.id.text_user_name);
+        String admin = SharedPrefManager.getInstance(this).getAdmin().getName();
+        UserName.setText("مرحبا ! " + admin);
 
         teach_cardView.setOnClickListener(v -> {
             Intent i = new Intent(getApplicationContext(), TeacherActivity.class);
