4dcb77863c268ce6ae52cf348d5c1f554ed6a0df
Ahmed K Madani <ahmedkamal543@gmail.com>
2020-12-30 18:29:23 +0200

    add teacher activity fixed

--- app/src/main/java/com/example/quranapp/AddingTeacher.java
+++ app/src/main/java/com/example/quranapp/AddingTeacher.java
@@ -3,10 +3,12 @@ package com.example.quranapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -31,11 +33,24 @@ public class AddingTeacher extends AppCompatActivity {
     String teacher_phone_;
     String teacher_password_;
 
+    ViewDialog viewDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.adding_teacher);
 
+        viewDialog = new ViewDialog(this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_teach);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("اضافة شيخ");
+
+        toolbar.setNavigationOnClickListener(v -> {
+            finish();
+        });
 
         teacher_name_editText = findViewById(R.id.teacher_name_editText);
         teacher_living_editText = findViewById(R.id.teacher_living_editText);
@@ -43,16 +58,20 @@ public class AddingTeacher extends AppCompatActivity {
         teacher_password_editText = findViewById(R.id.teacher_password_editText);
 
         Button addTeacherButton = findViewById(R.id.add_teacher_button);
-
         addTeacherButton.setOnClickListener(v -> AddTeacher());
     }
 
     private void AddTeacher() {
 
+        viewDialog.showDialog();
         teacher_name_ = teacher_name_editText.getText().toString();
         teacher_living_ = teacher_living_editText.getText().toString();
         teacher_phone_ = teacher_phone_editText.getText().toString();
         teacher_password_ = teacher_password_editText.getText().toString();
+
+        if (!validate()) {
+            return;
+        }
 
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URLs.AddTeacher + "?IdAdmin=" + SharedPrefManager.getInstance(this).getAdmin().getId() + "&Name=" + teacher_name_ + "&Password=" + teacher_password_
@@ -70,7 +89,6 @@ public class AddingTeacher extends AppCompatActivity {
                     }
 
                     Log.d("String Response : ", "" + response.toString());
-                    Log.d("name", String.valueOf(SharedPrefManager.getInstance(this).isLoggedIn()));
                 }, error -> Log.d("Error getting response", "" + error));
 
         requestQueue.add(jsonObjectRequest);
@@ -79,7 +97,7 @@ public class AddingTeacher extends AppCompatActivity {
     }
 
     private void onInsertFailed() {
-//        viewDialog.hideDialog();
+        viewDialog.hideDialog();
         Snackbar.make(findViewById(android.R.id.content), "Sign in Failed", Snackbar.LENGTH_LONG)
                 .setAction("Try Again", v -> {
                     AddTeacher();
@@ -87,10 +105,44 @@ public class AddingTeacher extends AppCompatActivity {
     }
 
     private void onInsertSuccess() {
-//        viewDialog.hideDialog();
+        viewDialog.hideDialog();
         Snackbar.make(findViewById(android.R.id.content), "Sign in Successfully", Snackbar.LENGTH_LONG)
                 .show();
         startActivity(new Intent(this, TeacherActivity.class));
         finish();
+    }
+
+    private boolean validate() {
+        boolean valid = true;
+
+        if (teacher_name_.length() == 0 ) {
+            teacher_name_editText.setError("الرجاء ادخال رقم هاتف صالح");
+            valid = false;
+        } else {
+            teacher_name_editText.setError(null);
+        }
+
+        if (teacher_living_.length()  == 0) {
+            teacher_living_editText.setError("كلمة السر يجب ان تحتوي علي اربعة حروف علي الاقل ");
+            valid = false;
+        } else {
+            teacher_living_editText.setError(null);
+        }
+
+        if (teacher_phone_.length()  == 0) {
+            teacher_phone_editText.setError("كلمة السر يجب ان تحتوي علي اربعة حروف علي الاقل ");
+            valid = false;
+        } else {
+            teacher_phone_editText.setError(null);
+        }
+
+        if (teacher_password_.length()  == 0) {
+            teacher_password_editText.setError("كلمة السر يجب ان تحتوي علي اربعة حروف علي الاقل ");
+            valid = false;
+        } else {
+            teacher_password_editText.setError(null);
+        }
+
+        return valid;
     }
 }
