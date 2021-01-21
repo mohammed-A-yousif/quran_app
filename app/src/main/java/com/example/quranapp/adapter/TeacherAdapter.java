38897d483a03220d778c364db0feaa7233a95e72
mohamed-A-ahmed <moh999ah@gmail.com>
2021-01-21 22:11:41 +0200

    share card content to edit/delete!

--- app/src/main/java/com/example/quranapp/adapter/TeacherAdapter.java
+++ app/src/main/java/com/example/quranapp/adapter/TeacherAdapter.java
@@ -1,5 +1,6 @@
 package com.example.quranapp.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -84,6 +85,7 @@ public class TeacherAdapter extends RecyclerView.Adapter<TeacherAdapter.ViewHold
 //       end of new
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Teacher listItem = listItems.get(position);
